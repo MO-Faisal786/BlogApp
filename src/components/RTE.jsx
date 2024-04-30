5894c2145652866d131d9d0342e13ae1239830f9
MO-Faisal786 <fm704034@gmail.com>
2024-04-30 18:36:59 +0530

    first commit

---
+++ src/components/RTE.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
+
+
+export default function RTE({label, name, control, defaultValue =""}) {
+  return (
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+        <Editor
+        apiKey='xeb9227frlq7lfq0xmgec2rc96z08643qt3k5wzcao5a67n5'
+        initialValue={defaultValue}
+        init={{
+            initialValue: defaultValue,
+            height: 500,
+            menubar: true,
+            plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+            ],
+            toolbar:
+            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }}
+        onEditorChange={onChange}
+        />
+    )}
+    />
+
+     </div>
+  )
+}
+
---
+++ src/pages/Post.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import { Button, Container } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
+
+export default function Post() {
+    const [post, setPost] = useState(null);
+    const { slug } = useParams();
+    const navigate = useNavigate();
+
+    const userData = useSelector((state) => state.auth.userData);
+
+    const isAuthor = post && userData ? post.userid === userData.$id : false;
+
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug).then((post) => {
+                if (post) setPost(post);
+                else navigate("/");
+            });
+        } else navigate("/");
+    }, [slug, navigate]);
+
+    const deletePost = () => {
+        appwriteService.deletePost(post.$id).then((status) => {
+            if (status) {
+                appwriteService.deleteFile(post.featuredimage);
+                navigate("/");
+            }
+        });
+    };
+
+    return post ? (
+        <div className="py-8">
+            <Container>
+                <div className="w-3/4 flex justify-evenly m-auto mb-4 relative border rounded-md p-2">
+                    <div className="w-1/2">
+                        <img
+                            src={appwriteService.getFilePreview(post.featuredimage)}
+                            alt={post.title}
+                            className="rounded-xl"
+                        />
+                        {isAuthor && (
+                            <div className="absolute right-6 top-6">
+                                <Link to={`/edit-post/${post.$id}`}>
+                                    <Button bgColor="bg-green-500" className="mr-3">
+                                        Edit
+                                    </Button>
+                                </Link>
+                                <Button bgColor="bg-red-500" onClick={deletePost}>
+                                    Delete
+                                </Button>
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex flex-col justify-center items-start px-10  w-1/2">
+                            <h1 className="text-2xl mb-6 font-bold">Title: <span className="text-orange-900">{post.title}</span></h1>
+                            {parse(post.content)}
+                    </div>
+                </div>
+            </Container>
+        </div>
+    ) : null;
+}
